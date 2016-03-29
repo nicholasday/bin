79b4a7fa9c41413873b68639c4289206d60b366d
Nicholas Day <nick@nickendo.com>
2016-03-29 13:32:53 -0400

    Added sleep of 3 and titled shell Mopidy to music.sh

--- music.sh
+++ music.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-i3-msg "workspace $1; exec urxvt -e bash -c \"~/src/bin/run.sh mopidy\""
-i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/music/ && sleep 2 && ~/src/bin/run.sh random\""
+i3-msg "workspace $1; exec urxvt -title Mopidy -e bash -c \"~/src/bin/run.sh mopidy\""
+i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/music/ && sleep 3 && ~/src/bin/run.sh random\""
