257a2779755348f39a42068a6f0a2b3e97ad1f5d
Nicholas Day <nick@nickendo.com>
2016-03-29 13:37:04 -0400

    Added window name to reacttest-edit.sh

--- reacttest-edit.sh
+++ reacttest-edit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-i3-msg "workspace $@; exec urxvt -e bash -c \"cd ~/src/reacttest && ~/src/bin/run.sh nvim app/*.js app/components/*\""
+i3-msg "workspace $@; exec urxvt -title 'Reacttest - editor' -e bash -c \"cd ~/src/reacttest && ~/src/bin/run.sh nvim app/*.js app/components/*\""
