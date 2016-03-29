0e2ba4edc4af0f67d06850bd7763bdd0ac4869d6
Nicholas Day <nick@nickendo.com>
2016-03-29 13:36:34 -0400

    Added names to phoenixnowbackend.sh windows

--- phoenixnowbackend.sh
+++ phoenixnowbackend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/src/PhoenixNowBackend/ && ~/src/bin/run.sh gunicorn app --reload\""
+i3-msg "workspace $1; exec urxvt -title 'PhoenixNowBackend - gunicorn' -e bash -c \"cd ~/src/PhoenixNowBackend/ && ~/src/bin/run.sh gunicorn app --reload\""
 i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/src/PhoenixNowBackend/ && bash\""
-i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/src/PhoenixNowBackend/ && ~/src/bin/run.sh nvim *.py\""
+i3-msg "workspace $1; exec urxvt -title 'PhoenixNowBackend - editor' -e bash -c \"cd ~/src/PhoenixNowBackend/ && ~/src/bin/run.sh nvim *.py\""
