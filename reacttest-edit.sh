1b4e3793d4c595ad248ca4eed5ac434753f71875
Nicholas Day <nick@nickendo.com>
2016-12-18 17:33:59 -0500

    changes

--- reacttest-edit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-i3-msg "workspace $@; exec urxvt -title 'Reacttest - editor' -e bash -c \"cd ~/src/reacttest && ~/src/bin/run.sh nvim app/*.js app/components/*\""
--- reacttest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-i3-msg "workspace $1; exec urxvt -title 'Reacttest - webpack' -e bash -c \"cd ~/src/reacttest/ && ~/src/bin/run.sh npm run serve\""
-i3-msg "workspace $1; exec urxvt -e bash -c \"cd ~/src/reacttest/ && bash\""
-. ~/src/bin/reacttest-edit.sh "$1"
