3c58bd80118d3b5ce15f59a97df5d284129a3134
Nicholas Day <nick@tutorialsbynick.com>
2018-05-18 14:09:39 -0400

    Edited lapis-one

--- lapis-one.sh
+++ lapis-one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo systemctl start postgresql
-i3-msg 'exec "alacritty -t \"yarn start\" --working-directory ~/src/lapis-one/frontend -e ~/src/bin/run.sh yarn start"'
-i3-msg 'exec "alacritty -t \"watchexec\" --working-directory ~/src/lapis-one -e ~/.cargo/bin/watchexec -w ~/src/lapis-one -r ~/.cargo/bin/cargo run"'
-i3-msg 'workspace 3; exec zim'
-i3-msg 'workspace 1; exec code ~/src/lapis-one'
+sudo sysctl fs.inotify.max_user_watches=524288
+i3-msg 'workspace 2; exec zim'
+i3-msg 'workspace 1; exec "~/src/bin/run.sh code ~/src/lapis-one"'
+i3-msg 'exec "~/src/bin/run.sh code ~/src/lapis-one/frontend"'
